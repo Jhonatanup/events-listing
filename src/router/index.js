395da2626fc8c92291a3ea4d47c8a7415a492568
jhonatanup <jleandroweb@gmail.com>
2020-03-16 14:33:26 -0400

    dinamic routing & history mode

--- src/router/index.js
+++ src/router/index.js
@@ -13,9 +13,10 @@ const routes = [
     component: EventList
   },
   {
-    path: '/event',
+    path: '/event/:id',
     name: 'event-show',
-    component: EventShow
+    component: EventShow,
+    props: true
   },
   {
     path: '/event/create',
@@ -25,7 +26,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+    // eslint-disable-next-line prettier/prettier
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Show Event #1</router-link
+    >
+    |
   </div>
 </template>
