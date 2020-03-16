03939d0e9ec80327781ffa41dbc3fbe226a49c13
jhonatanup <jleandroweb@gmail.com>
2020-03-16 15:17:12 -0400

    single file vue components

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    // eslint-disable-next-line prettier/prettier
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Show Event #1</router-link
-    >
-    |
+    <EventCard />
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
