d0b35c52e2cca421a0aae406ca7884d627f46842
jhonatanup <jleandroweb@gmail.com>
2020-03-17 00:24:02 -0400

    eventList vuex

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -6,28 +6,39 @@
       :key="event.id"
       :event="event"
     ></EventCard>
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Previous Page |</router-link
+      >
+    </template>
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page</router-link
+    >
   </div>
 </template>
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        ;(this.events = response.data), console.log(response.data)
-      })
-      .catch(error => {
-        console.log(error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events'])
   }
 }
 </script>
